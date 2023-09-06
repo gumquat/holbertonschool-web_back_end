be14673529ac97ae03fef466020811a830028530
gumquat <gumquat@gmail.com>
2023-09-06 12:58:55 -0500

    formatting

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-
+    const task = true;
+    const task2 = false;
+    return [task2, task];
   }
 
   return [task, task2];
