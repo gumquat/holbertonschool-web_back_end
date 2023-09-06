22b19bf763d1b281e6daddacec0c9f442c320b42
gumquat <gumquat@gmail.com>
2023-09-06 13:02:44 -0500

    formatting

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {  
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
-};
+  };
 
   return budget;
 }
