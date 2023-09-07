b2322f434eca2062af163f80e68c37908166247d
gumquat <gumquat@gmail.com>
2023-09-07 15:04:48 -0500

    revert

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,17 @@
-class Airport {
-  constructor(name, code) {
+export default class Airport {
+  cosntructor (name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError ('Expected a string')
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError ('Expected string')
+    }
+    
     this._name = name;
     this._code = code;
   }
 
-  toString() {
-    return this._code;
-  }
+    toString() {
+      return `[object ${this._code}]`;
+    }
 }
