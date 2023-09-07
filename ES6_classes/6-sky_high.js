a7a3035c5fa9f8b443db360d240a6cfe58eab6f6
gumquat <gumquat@gmail.com>
2023-09-07 14:51:37 -0500

    hoooray move that bracket over

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
@@ -14,5 +14,3 @@ class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
