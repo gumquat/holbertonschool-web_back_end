1f6c43995ec28e35f603021657631aa292945d5b
gumquat <gumquat@gmail.com>
2023-09-07 14:50:08 -0500

    WHYYY

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,16 +1,18 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
-}
+  }
 
   get floors() {
     return this._floors;
   }
 
-  evacuationWarningMEssage() {
-    return 'Evacuate slowly the ${this._floors} floors';
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+export default SkyHighBuilding;
