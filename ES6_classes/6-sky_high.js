c5511815c56b0d9a7e66c4617bd1bf24e38ce0fd
gumquat <gumquat@gmail.com>
2023-09-07 14:33:32 -0500

    silly me i forgot to push

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Currency from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+}
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMEssage() {
+    return 'Evacuate slowly the ${this._floors} floors';
+  }
+}
