da62124dd8983b7f66d3a54af2ea4cf35e87665e
gumquat <gumquat@gmail.com>
2023-09-07 12:07:08 -0500

    const to let

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,13 +1,13 @@
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  const rooms = [];
+  let rooms = [];
 
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
 
-  rooms = (room1, room2, room3);
+  rooms.push(room1, room2, room3);
 
   return rooms;
 }
