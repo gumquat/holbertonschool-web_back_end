e82b413e5c97d325f793922b7b1450c50582e25a
gumquat <gumquat@gmail.com>
2023-09-07 14:40:47 -0500

    export default at the end?

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,9 +1,8 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._sqft = sqft;
     this._floors = floors;
 }
 
@@ -15,3 +14,5 @@ export default class SkyHighBuilding extends Building {
     return 'Evacuate slowly the ${this._floors} floors';
   }
 }
+
+export default SkyHighBuilding;
