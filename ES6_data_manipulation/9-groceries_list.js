5b352298e2224be60b9bc99c3423f2e321de7bcb
gumquat <gumquat@gmail.com>
2023-09-12 14:44:46 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
-function groceriesList() {
-  const groceries = new Map();
-
-  groceries.set('Apples', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Banana', 5);
+export default function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 
   return groceries;
 }
