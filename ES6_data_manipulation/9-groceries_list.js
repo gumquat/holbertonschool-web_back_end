e1446966487e60e9c920060abfb92158359954b5
gumquat <gumquat@gmail.com>
2023-09-12 14:05:23 -0500

    numba niiine

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+  const groceryMap = new Map();
+  groceryMap.set('name1', 'quantity1');
+  groceryMap.set('name2', 'quantity2');
+  groceryMap.set('name3', 'quantity3');
+
+  return groceryMap;
+}
