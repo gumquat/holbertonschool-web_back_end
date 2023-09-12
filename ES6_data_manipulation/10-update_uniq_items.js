d0756dccfa9d6632ec855639fe771a44ee1218d0
gumquat <gumquat@gmail.com>
2023-09-12 14:58:21 -0500

    forgot to do imports lol

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,6 @@
+import updateUniqueItems from "./10-update_uniq_items.js";
+import groceriesList from "./9-groceries_list.js";
+
 function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
