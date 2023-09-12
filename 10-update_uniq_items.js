0a1637e8ac1110ffe180f92aa0088b34cbf063a1
gumquat <gumquat@gmail.com>
2023-09-12 14:52:46 -0500

    numbah teeeen

---
+++ 10-update_uniq_items.js
@@ -0,0 +1,11 @@
+//imports
+import updateUniqueItems from "./10-update_uniq_items.js";
+import groceriesList from "./9-groceries_list.js";
+
+// im the map im the map is the map im the map im the map im the map
+const imthemap = groceriesList();
+console.log(imthemap);
+
+//call that old function here
+updateUniqueItems(imthemap)
+console.log(imthemap);
