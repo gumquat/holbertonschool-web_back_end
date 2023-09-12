27528052a768ea121d5d842d9ab0988a138a4d55
gumquat <gumquat@gmail.com>
2023-09-12 12:54:33 -0500

    changes

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city)
+  const theList = students.filter((student) => student.location === city)
     .map((student) => {
       const gradeObject = newGrades.find((item) => item.studentId === student.id);
       const grade = gradeObject ? gradeObject.grade : 'N/A';
-      return { ...student, grade };
+      return theList;
     });
 }
