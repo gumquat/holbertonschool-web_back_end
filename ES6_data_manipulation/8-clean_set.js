32362e30cdf8c61f99cee84faf8742a4f0a3330a
gumquat <gumquat@gmail.com>
2023-09-12 13:36:19 -0500

    lint fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   if (!startString || startString.length < 1) return '';
-  
+
   return [...set]
-    .filter(it => it && it.startsWith(startString))
-    .map(it => it.substring(startString.length))
+    .filter((it) => it && it.startsWith(startString))
+    .map((it) => it.substring(startString.length))
     .join('-');
 }
