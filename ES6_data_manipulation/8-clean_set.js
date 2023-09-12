5f873086ea8b0427a287ead63dbb86184c6e2ba2
gumquat <gumquat@gmail.com>
2023-09-12 13:34:35 -0500

    numbah eeeight (maybe)

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length < 1) return '';
+  
+  return [...set]
+    .filter(it => it && it.startsWith(startString))
+    .map(it => it.substring(startString.length))
+    .join('-');
+}
