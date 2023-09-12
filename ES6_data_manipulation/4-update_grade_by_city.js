2ca6545d8a78292ed68bf035b19c6a6117a28b68
gumquat <gumquat@gmail.com>
2023-09-12 12:35:57 -0500

    oops return

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,4 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentGradeByCity = students.filter((student) => student.location === city).map((student) => )
+  const studentGradeByCity = students.filter((student) => student.location === city).map((student) => );
+  return studentGradeByCity;
 }
