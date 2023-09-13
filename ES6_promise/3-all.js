160386b9c8c791e3d72ce1de01913331365ac12b
gumquat <gumquat@gmail.com>
2023-09-13 10:42:00 -0500

    at least getting credit for present files lol

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
